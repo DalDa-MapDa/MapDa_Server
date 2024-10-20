0c3b3cf50a50642822f681ef80f46bd541a7adab
“LeeYuChul” <yoochul89@naver.com>
2024-10-21 01:16:22 +0900

    [TEST] 로거 수정

--- login/google_login.py
+++ login/google_login.py
@@ -1,15 +1,10 @@
 import os
-import logging
-from fastapi import APIRouter, HTTPException, Depends, Request
+from fastapi import APIRouter, HTTPException, Request
 import requests
 from pydantic import BaseModel
 from dotenv import load_dotenv
 
 router = APIRouter()
-
-# 로깅 설정
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 # .env 파일 로드
 load_dotenv()
@@ -24,8 +19,8 @@ class GoogleLoginData(BaseModel):
 
 @router.post("/login/google")
 async def google_login(data: GoogleLoginData, request: Request):
-    logger.info(f"Received Google login request from {request.client.host}")
-    logger.info(f"Request data: {data}")
+    print(f"Received Google login request from {request.client.host}")
+    print(f"Request data: {data}")
 
     # 1. Google OAuth 2.0 서버로부터 Access Token 요청
     token_url = "https://oauth2.googleapis.com/token"
@@ -37,27 +32,27 @@ async def google_login(data: GoogleLoginData, request: Request):
         'grant_type': 'authorization_code'
     }
 
-    logger.info(f"Sending token request to Google: {token_data}")
+    print(f"Sending token request to Google: {token_data}")
     
     token_response = requests.post(token_url, data=token_data)
     
     if token_response.status_code != 200:
-        logger.error(f"Google token request failed with status code: {token_response.status_code}, response: {token_response.text}")
+        print(f"Google token request failed with status code: {token_response.status_code}, response: {token_response.text}")
         raise HTTPException(status_code=token_response.status_code, detail="구글 인증 실패")
 
     token_info = token_response.json()
-    logger.info(f"Google token response: {token_info}")
+    print(f"Google token response: {token_info}")
     
     # 2. Access Token을 사용하여 사용자 정보 요청
     user_info_url = "https://www.googleapis.com/oauth2/v2/userinfo"
     user_info_response = requests.get(user_info_url, headers={"Authorization": f"Bearer {token_info['access_token']}"})
     
     if user_info_response.status_code != 200:
-        logger.error(f"Google user info request failed with status code: {user_info_response.status_code}, response: {user_info_response.text}")
+        print(f"Google user info request failed with status code: {user_info_response.status_code}, response: {user_info_response.text}")
         raise HTTPException(status_code=user_info_response.status_code, detail="사용자 정보 요청 실패")
 
     user_info = user_info_response.json()
-    logger.info(f"Google user info response: {user_info}")
+    print(f"Google user info response: {user_info}")
     
     # 3. 사용자 정보 반환
     return {"message": "구글 로그인 성공", "user_info": user_info}
